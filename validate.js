a39c0a9b74c37010c8fbb4ec09835cb1ba5d5cad
Ghad <zimobie@gmail.com>
2020-04-09 20:21:41 +0100

    corrected novalidate syntax

--- validate.js
+++ validate.js
@@ -3,7 +3,7 @@ const emailField = document.querySelector('input[type="email"]');
 /* disable native form validation in js so that if script fails,
   native validation kicks in */
 const form = document.querySelector('.validate');
-form.setAttribute('novalidate', true);
+form.setAttribute('novalidate');
 
 // Validate the field
 const hasError = field => {
